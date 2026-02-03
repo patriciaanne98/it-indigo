5cefbaffa06e5ee5d751d8d2b68d8c7ec5530046
patriciaanne98 <147879232+patriciaanne98@users.noreply.github.com>
2026-02-03 05:03:50 +0000

    magic circle

--- circle-project/js/script.js
+++ circle-project/js/script.js
@@ -1,3 +1,31 @@
-document.getElementById("radius")
+function calcDiameter(radius) {
+  return 2 * radius;
+}
 
-document.getElementById("diameterOut").textContent = ...
+function calcCircumference(radius) {
+  return 2 * Math.PI * radius;
+}
+
+function calcArea(radius) {
+  return Math.PI * radius * radius;
+}
+
+document.getElementById("calculateBtn").onclick = function () {
+  let radiusString = document.getElementById("radius").value;
+  let radius = parseFloat(radiusString);
+
+  let diameter = calcDiameter(radius);
+  let circumference = calcCircumference(radius);
+  let area = calcArea(radius);
+
+  document.getElementById("diameterOut").textContent = diameter;
+  document.getElementById("circumferenceOut").textContent = circumference;
+  document.getElementById("areaOut").textContent = area;
+};
+
+document.getElementById("clearBtn").onclick = function () {
+  document.getElementById("radius").value = "";
+  document.getElementById("diameterOut").textContent = "";
+  document.getElementById("circumferenceOut").textContent = "";
+  document.getElementById("areaOut").textContent = "";
+};
